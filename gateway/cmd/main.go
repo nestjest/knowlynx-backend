4ff4c4d7057b5b228a3c0b5d8c54c65c983b7afc
nestjest <kockers@vk.com>
2026-05-12 10:58:26 +0300

    new initial commit2

---
+++ gateway/cmd/main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	//TODO: Инициализировать сервер для HTTP Gateway.
+	//TODO: Инициализировать методы микросервисов.
+	//TODO: Реализовать Graceful shutdown
+	//TODO: Написать тесты для HTTP Gateway.
+}
